c9f48e72ea65f82cdf8cb8ff41d440ee1e95878d
First Name <you@example.com>
2017-03-07 04:00:42 +0530

    sql-profile linked

--- loginclick.php
+++ loginclick.php
@@ -1,10 +1,7 @@
-<html>
-<head>
-<title>
-</title>
-</head>
-<body>
+
 <?php
+session_start();
+
 include('connection.php');
 //include('login.php');
 if($_SERVER['REQUEST_METHOD'] == "POST")
@@ -18,15 +15,39 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
     $result = $conn->query($sqltest);
     if($result->num_rows == 1)
     {
-     header("Location:http://192.168.121.187:8001/surya/profile.php "); /* Redirect browser */
+      $_SESSION["USERNAME"]=$o;
+
+      
+      
+     $sql = "SELECT NAME,NUMBER  FROM php_assign where USER_NAME='$o' AND PASSWORD='$m'";
+     $result1 = $conn->query($sql);
+
+     if ($result1->num_rows > 0) {
+           // output data of each row
+           while($row = $result1->fetch_assoc()) {
+                    echo $row["NcAME"];
+                     $_SESSION["NAME"]=$row["NAME"];
+                     $_SESSION["NUMBER"]=$row["NUMBER"];
+
+           }
+     } else {
+           echo "0 results";
+     }
+     $conn->close();
+      header("Location:http://192.168.121.187:8001/surya/profile.php "); /* Redirect browser */
+
+    
      exit();
     }
-    else
-    {
-      echo"wrong";
-    }
+   
   }
 }
 ?>
+<html>
+<head>
+<title>
+</title>
+<body>
 </body>
+
 </html>
