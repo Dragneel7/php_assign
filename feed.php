5708e08240c7bd59a67571805b69e945233eda86
First Name <you@example.com>
2017-03-08 00:18:15 +0530

    common feed page added

--- feed.php
+++ feed.php
@@ -7,50 +7,33 @@ session_start();
     <title></title>
     </head>
     <body>
-    <?php
-include'connection.php';
-if($_SERVER['REQUEST_METHOD']==='POST'){
-if(empty($_POST["feed"])){  
-$x="field is empty";
-    }
-else{ 
-  $m = test_input($_POST["feed"]); 
-
-if(isset($m)){
-$sql="insert into feeds(USER_FEED)
-values('$m')";
-if($conn->query(sql)===true){
-echo"feed added";
-}
-else{ 
-echo"error".$sql."<br>".mysqli_error($conn);
-}
-mysqli_close($conn);
-}
-    }
-}
-
-function test_input($data) {
-    $data = trim($data);
-      $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-          return $data;
-}
-    ?>
+    
     <div id="header">
     <h1>COMMON FEED GROUND</h1>
     </div>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-    <textarea rows="25" cols="140" name="feed_del" form="post"></textarea>
     <div class="feed"><span><?php if(isset($x)){echo $x;}?></span>
-    <textarea rows="2" cols="120" name="feed" form="post"></textarea>
-    
+    <p id="add"><?php 
+    include'connection.php';
+   $sql="select * from feeds order by DATE";
+   $result= $conn->query($sql);
+   if($result->num_rows>0){
+while($row=$result->fetch_assoc()){  
+echo $row["USER_NAME"]."(".$row["DATE"].") : ".$row["USER_FEED"]."<br>";
+
+}
+   }
+   
+   else{
+     echo"0 result";
+   }
+   $conn->close();
+   
+   ?></p>
+    <form method="post" action="fillfeed.php">
+    <textarea rows="2" cols="120" name="feed"></textarea> 
     <input type="submit">
     </form>
-    <form method="post" action="completeprofile.php">
-    <input type="submit" value="view profile">
-    </form>
-    
+    <a href="completeprofile.php">View profile</a>
     </div>
     </body>
     </html>
---
+++ fillfeed.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+?>
+<html>
+<head>
+<title>
+</title>
+</head>
+<body>
+<?php
+include'connection.php';
+$name=$_SESSION["USERNAME"];
+$feed=$_POST["feed"];
+$sql="insert into feeds (USER_FEED,USER_NAME) value('$feed','$name')";
+if ($conn->query($sql) === TRUE) {
+      echo "New record created successfully";
+} else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+$_SESSION["FEED"]=$feed;
+echo $_SESSION["FEED"];
+echo $_SESSION["USERNAME"];
+ header("Location:http://192.168.121.187:8001/surya/feed.php ");
+?>
+</body>
+</html>
